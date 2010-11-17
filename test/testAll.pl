d9a56c68a1fb1aada5efe7678a6dbc8705ed674a
Marty Kraimer <mrkraimer@comcast.net>
2010-11-17 11:19:22 -0500

    added testThread to testAll

--- test/testAll.pl
+++ test/testAll.pl
@@ -2,8 +2,9 @@ eval 'exec perl -S $0 ${1+"$@"}'  # -*- Mode: perl -*-
     if $running_under_some_shell; # testIntTypes.pl
 use Env;
 system ("./testPVType.pl");
+system ("./testThread.pl");
+system ("./testLinkedList.pl");
 system ("./testIntrospect.pl");
 system ("./testPVData.pl");
 system ("./testPVStructureArray.pl");
 system ("./testPVAuxInfo.pl");
-system ("./testLinkedList.pl");
