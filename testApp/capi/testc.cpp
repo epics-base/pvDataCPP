3291b6a8f2a202f3e99e6996156c04f375d15ab7
jrowlandls <unknown>
2012-08-01 16:48:10 +0100

    commit to branch to continue python api while C++ api is in flux

--- testApp/capi/testc.cpp
+++ testApp/capi/testc.cpp
@@ -1,8 +1,20 @@
+#include <iostream>
+
 #include <pv/pvData.h>
 #include <pv/standardField.h>
 #include <pv/standardPVField.h>
 
 using namespace epics::pvData;
+
+struct StructureHandle;
+
+struct FieldVariant
+{
+    char * name;
+    Type t;
+    ScalarType st;
+    StructureHandle * sh;
+};
 
 struct StructureHandle
 {
@@ -10,13 +22,114 @@ struct StructureHandle
     StructureHandle(StructureConstPtr s) : s(s) {}
 };
 
+struct PVStructureHandle
+{
+    PVStructurePtr s;
+    PVStructureHandle(PVStructurePtr s) : s(s) {}
+};
+
+template<typename T> void putField(void * handle, char * name, T value)
+{
+    PVStructureHandle * ph = (PVStructureHandle *)handle;
+    std::tr1::shared_ptr<PVScalarValue<T> > a = 
+        std::tr1::static_pointer_cast<PVScalarValue<T> >(ph->s->getSubField(name));
+    a->put(value);
+}
+
+#define MAKEputField(T) \
+    void putField_##T(void * handle, char * name, T value) \
+    {                                                     \
+        putField(handle, name, value);                    \
+    }
+
 extern "C"
 {
+    double * getDoubleArray(void * handle, char * name)
+    {
+        PVStructureHandle * ph = (PVStructureHandle *)handle;
+        PVScalarArrayPtr p = ph->s->getScalarArrayField(name, pvDouble);
+        PVDoubleArrayPtr d = std::tr1::static_pointer_cast<PVDoubleArray>(p);
+        return d->get();
+    }
+    int getLength(void * handle, char * name)
+    {
+        PVStructureHandle * ph = (PVStructureHandle *)handle;
+        PVFieldPtr p = ph->s->getSubField(name);
+        PVScalarArrayPtr a = std::tr1::static_pointer_cast<PVScalarArray>(p);
+        return a->getLength();
+    }
+    void setLength(void * handle, char * name, int length)
+    {
+        PVStructureHandle * ph = (PVStructureHandle *)handle;
+        PVFieldPtr p = ph->s->getSubField(name);
+        PVScalarArrayPtr a = std::tr1::static_pointer_cast<PVScalarArray>(p);
+        return a->setLength(length);
+    }
+    
+    int getInt(void * handle, char * name)
+    {
+        PVStructureHandle * ph = (PVStructureHandle *)handle;
+        return ph->s->getIntField(name)->get();
+    }
+
+    MAKEputField(int);
+    MAKEputField(double);
+    MAKEputField(short);
+    
+    void PVStructuretoString(void * handle)
+    {
+        PVStructureHandle * ph = (PVStructureHandle *)handle;
+        std::string text;
+        ph->s->toString(&text);
+        std::cout << text << std::endl;
+    }
+    
+    void * createPVStructure(void * handle)
+    {
+        StructureHandle * sh = (StructureHandle *)handle;
+        PVDataCreatePtr pvDataCreate = getPVDataCreate();
+        PVStructurePtr sp = pvDataCreate->createPVStructure(sh->s);
+        PVStructureHandle * ph = new PVStructureHandle(sp);
+        return (void *)ph;
+    }
+
+    void * createStructureVariant(FieldVariant * fieldVars, int length)
+    {
+        FieldCreatePtr fieldCreate = getFieldCreate();
+        FieldConstPtrArray fields;
+        StringArray fieldNames;
+        for(int n = 0; n < length; n++)
+        {
+            fieldNames.push_back(fieldVars[n].name);
+            ScalarType t = (ScalarType)fieldVars[n].st;
+            Type bt = (Type)fieldVars[n].t;
+            if(bt == scalarArray)
+            {
+                fields.push_back(fieldCreate->createScalarArray(t));
+            }
+            else if(bt == scalar)
+            {
+                fields.push_back(fieldCreate->createScalar(t));
+            }
+            else if(bt == structure)
+            {
+                fields.push_back(fieldVars[n].sh->s);
+            }
+            else if(bt == structureArray)
+            {
+                fields.push_back(fieldCreate->createStructureArray(fieldVars[n].sh->s));
+            }
+        }
+        StructureConstPtr s = fieldCreate->createStructure(fieldNames, fields);
+        StructureHandle * sh = new StructureHandle(s);
+        return (void *)sh;
+    }
+
     void * createTest()
     {
         FieldCreatePtr fieldCreate = getFieldCreate();
         PVDataCreatePtr pvDataCreate = getPVDataCreate();
-        FieldConstPtrArray  fields;
+        FieldConstPtrArray fields;
         StringArray fieldNames;
         fields.push_back(fieldCreate->createScalar(pvString));
         fields.push_back(fieldCreate->createScalarArray(pvDouble));
@@ -26,7 +139,7 @@ extern "C"
         StructureHandle * sh = new StructureHandle(s);
         return (void *)sh;
     }
-
+    
     int getNumberFields(void * handle)
     {
         StructureHandle * sh = (StructureHandle *)handle;
@@ -39,17 +152,16 @@ extern "C"
         return sh->s->getFieldNames()[n].c_str();
     }
 
-    int getFieldType(void * handle, const char * name)
+    int getFieldType(void * handle, int n)
     {
         StructureHandle * sh = (StructureHandle *)handle;
-        /* api missing const for getField(size_t) so have to use name */
-        return sh->s->getField(name)->getType();
+        return sh->s->getField(n)->getType();
     }
 
-    int getScalarType(void * handle, const char * name)
+    int getScalarType(void * handle, int n)
     {
         StructureHandle * sh = (StructureHandle *)handle;
-        FieldConstPtr f = sh->s->getField(name);
+        FieldConstPtr f = sh->s->getField(n);
         if(f->getType() == scalar)
         {
             ScalarConstPtr sp = std::tr1::static_pointer_cast<const Scalar>(f);
@@ -61,10 +173,10 @@ extern "C"
         }
     }
 
-    int getElementType(void * handle, const char * name)
+    int getElementType(void * handle, int n)
     {
         StructureHandle * sh = (StructureHandle *)handle;
-        FieldConstPtr f = sh->s->getField(name);
+        FieldConstPtr f = sh->s->getField(n);
         if(f->getType() == scalarArray)
         {
             ScalarArrayConstPtr sp = std::tr1::static_pointer_cast<const ScalarArray>(f);
@@ -83,5 +195,3 @@ extern "C"
     }
 
 };
-
-
--- testApp/capi/testc.py
+++ testApp/capi/testc.py
@@ -1,30 +1,65 @@
 #!/usr/bin/env python
 
+"pvData Python ctypes test with numpy array sharing"
+
 import os
 from ctypes import *
+from numpy import *
 
-# cytpes API
+# ctypes API
 
 libname = "../../lib/%s/libtestc.so" % os.environ["EPICS_HOST_ARCH"]
 lib = CDLL(libname)
 lib.createTest.restype = c_void_p
 lib.createTest.argtypes = []
+lib.getDoubleArray.argtypes = [c_void_p, c_char_p]
+lib.getDoubleArray.restype = POINTER(c_double)
+lib.getLength.argtypes = [c_void_p, c_char_p]
+lib.getLength.restype = c_int
+lib.setLength.argtypes = [c_void_p, c_char_p, c_int]
+lib.setLength.restype = None
+lib.getInt.argtypes = [c_void_p, c_char_p]
+lib.getInt.restype = c_int
+lib.putField_int.argtypes = [c_void_p, c_char_p, c_int]
+lib.putField_int.restype = None
+lib.putField_double.argtypes = [c_void_p, c_char_p, c_double]
+lib.putField_double.restype = None
+lib.PVStructuretoString.restype = None
+lib.PVStructuretoString.argtypes = [c_void_p]
+lib.createStructureVariant.restype = c_void_p
+lib.createStructureVariant.argtypes = [c_void_p, c_int]
+lib.createPVStructure.restype = c_void_p
+lib.createPVStructure.argtypes = [c_void_p]
 lib.getNumberFields.restype = c_int
 lib.getNumberFields.argtypes = [c_void_p]
 lib.getFieldName.restype = c_char_p
 lib.getFieldName.argtypes = [c_void_p, c_int]
 lib.getFieldType.restype = c_int
-lib.getFieldType.argtypes = [c_void_p, c_char_p]
+lib.getFieldType.argtypes = [c_void_p, c_int]
 lib.getScalarType.restype = c_int
-lib.getScalarType.argtypes = [c_void_p, c_char_p]
+lib.getScalarType.argtypes = [c_void_p, c_int]
 lib.getElementType.restype = c_int
-lib.getElementType.argtypes = [c_void_p, c_char_p]
+lib.getElementType.argtypes = [c_void_p, c_int]
+
+class FieldVariant(Structure):
+    _fields_ = (("name", c_char_p),
+                ("type", c_int),
+                ("scalarType", c_int),
+                ("structure", c_void_p))
 
 # Python API
 
+scalar, scalarArray, structure, structureArray = range(4)
 type_enum = ["scalar", "scalarArray", "structure", "structureArray"]
+scalar_enum = ["pvBoolean", "pvByte", "pvShort", "pvInt",
+               "pvLong", "pvUByte", "pvUShort", "pvUInt",
+               "pvULong", "pvFloat", "pvDouble", "pvString"]
 
-class Structure:
+pvBoolean, pvByte, pvShort, pvInt, \
+           pvLong, pvUByte, pvUShort, pvUInt, \
+           pvULong, pvFloat, pvDouble, pvString = range(12)
+
+class Structure4(object):
     def getNumberFields(self):
         return lib.getNumberFields(self.handle)
     def getFieldName(self, n):
@@ -38,20 +73,106 @@ class Structure:
     def __enter__(self):
         return self
     def __exit__(self, a, b, c):
-        print "destroying handle"
+        print "destroying Structure handle"
         lib.destroy(self.handle)
+    def init(self):
+        s = self
+        self.fields = []
+        # cache introspection in python
+        l = s.getNumberFields()
+        for n in range(l):
+            name = s.getFieldName(n)
+            type_ = s.getFieldType(n)
+            if type_enum[type_] == "scalar":
+                subtype = s.getScalarType(n)
+            elif type_enum[type_] == "scalarArray":
+                subtype = s.getElementType(n)
+            self.fields.append((name, type_, subtype))
+        
+    def show(self):
+        s = self
+        l = s.getNumberFields()
+        print "structure"
+        for n in range(l):
+            name = s.getFieldName(n)
+            type_ = s.getFieldType(n)
+            if type_enum[type_] == "scalar":
+                sc_type = s.getScalarType(n)
+                print "  ", scalar_enum[sc_type], name
+            elif type_enum[type_] == "scalarArray":
+                el_type = s.getElementType(n)
+                print "  ", scalar_enum[el_type], "[]", name
+            else:
+                print "TODO"
 
-class TestStructure(Structure):
-    def __init__(self):
-        self.handle = lib.createTest()
 
-with TestStructure() as s:
-    l = s.getNumberFields()
-    print "pvData structure has %d fields" % l
-    for n in range(l):
-        name = s.getFieldName(n)
-        type_ = s.getFieldType(name)
-        sc_type = s.getScalarType(name)
-        el_type = s.getElementType(name)
-        print name, type_enum[type_], sc_type, el_type
+class MyStructure(Structure4):
+    def __init__(self, fields = None):
+        if fields is None:
+            fields = self._fields_
+        N = len(fields)
+        fs = (FieldVariant * N)()
+        for n, (name, type_, subType) in enumerate(fields):
+            fs[n].name = name
+            fs[n].type = type_
+            if type_ == structure or type_ == structureArray:
+                fs[n].structure = subType().handle
+            else:
+                fs[n].scalarType = subType
+        self.handle = lib.createStructureVariant(fs, N)
+        self.init()
+
+class PVStructure(object):
+    def __init__(self, s):
+        self.handle = lib.createPVStructure(s.handle)
+    def show(self):
+        lib.PVStructuretoString(self.handle)
+    def __enter__(self):
+        return self
+    def __exit__(self, a, b, c):
+        print "destroying PVStructure handle (TODO)"
+        pass
+    def getInt(self, name):
+        return lib.getInt(self.handle, name)
+    def putInt(self, name, value):
+        return lib.putField_int(self.handle, name, value)
+    def putDouble(self, name, value):
+        return lib.putField_double(self.handle, name, value)
+    def setLength(self, name, length):
+        lib.setLength(self.handle, name, length)
+    def getDoubleArray(self, name):
+        sz = lib.getLength(self.handle, name)
+        ptr = lib.getDoubleArray(self.handle, name)
+        # use the library routine 'as_array' in numpy 1.6
+        ptr.__array_interface__ = {'typestr': '<f8',
+                                   'version': 3,
+                                   'data': (addressof(ptr.contents), False),
+                                   'shape': (sz,)}
+        a = array(ptr, copy = False)
+        return a
+    
+# create Structure from Python (ctypes style)
+
+class MyStructure1(MyStructure):
+    _fields_ = (("name", scalar, pvDouble),)
+
+class MyStructure2(MyStructure):
+    _fields_ = (("hello", scalar, pvDouble),
+                ("world", scalar, pvInt),
+                ("epics", scalarArray, pvUShort),
+                ("diamond", scalarArray, pvDouble),
+                ("xyz", structureArray, MyStructure1))
+
+with MyStructure2() as s:
+    s.show()
+    # create PVStructure from Structure
+    with PVStructure(s) as pv:
+        pv.setLength("diamond", 10)
+        a = pv.getDoubleArray("diamond")
+        a[:5] = -0.1
+        a[-5:] = [2,4,6,8,10]
+        a *= 100
+        pv.putInt("world", 99)
+        pv.putDouble("hello", 102.5)
+        pv.show()
         
