5b1b5ab904babbf6a5694d34df6df35ab1899787
Matej Sekoranja <unknown>
2014-10-31 18:29:55 +0100

    tests: refactored test building so that one test object can be created for VxWorks and RTEMS

--- testApp/pvDataAllTests.c
+++ testApp/pvDataAllTests.c
@@ -9,6 +9,11 @@
 #include <epicsThread.h>
 #include <epicsUnitTest.h>
 
+/* copy */
+int testCreateRequest(void);
+int testPVCopy(void);
+
+/* misc */
 int testBaseException(void);
 int testBitSet(void);
 int testByteBuffer(void);
@@ -22,10 +27,32 @@ int testTimeStamp(void);
 int testTimer(void);
 int testTypeCast(void);
 
+/* property */
+int testCreateRequest(void);
+
+/* pv */
+int testBitSetUtil(void);
+int testConvert(void);
+int testFieldBuilder(void);
+int testIntrospect(void);
+int testOperators(void);
+int testPVData(void);
+int testPVScalarArray(void);
+int testPVStructureArray(void);
+int testPVType(void);
+int testPVUnion(void);
+int testStandardField(void);
+int testStandardPVField(void);
+
 void epicsRunPVDataTests(void)
 {
     testHarness();
 
+    /* copy */
+    runTest(testCreateRequest);
+    runTest(testPVCopy);
+
+    /* misc */
     runTest(testBaseException);
     runTest(testBitSet);
     runTest(testByteBuffer);
@@ -38,4 +65,23 @@ void epicsRunPVDataTests(void)
     runTest(testTimeStamp);
     runTest(testTimer);
     runTest(testTypeCast);
+
+    /* property */
+    runTest(testCreateRequest);
+
+    /* pv */
+    runTest(testBitSetUtil);
+    runTest(testConvert);
+    runTest(testFieldBuilder);
+    runTest(testIntrospect);
+    runTest(testOperators);
+    runTest(testPVData);
+    runTest(testPVScalarArray);
+    runTest(testPVStructureArray);
+    runTest(testPVType);
+    runTest(testPVUnion);
+    runTest(testStandardField);
+    runTest(testStandardPVField);
+
 }
+
