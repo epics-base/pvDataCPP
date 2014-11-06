f36c8ce280b1634511a03c9e146f107aecc3cfdf
Matej Sekoranja <unknown>
2014-11-06 14:28:49 +0100

    testByteOrder: use MAIN instead of main

--- testApp/misc/testByteOrder.cpp
+++ testApp/misc/testByteOrder.cpp
@@ -1,7 +1,8 @@
+#include <testMain.h>
 #include <epicsEndian.h>
 #include <stdio.h>
 
-int main()
+MAIN(testByteOrder)
 {
 printf("EPICS_BYTE_ORDER: %s\n", (EPICS_BYTE_ORDER == EPICS_ENDIAN_LITTLE) ? "little" : "big");
 printf("EPICS_FLOAT_WORD_ORDER: %s\n", (EPICS_FLOAT_WORD_ORDER == EPICS_ENDIAN_LITTLE) ? "little" : "big");
