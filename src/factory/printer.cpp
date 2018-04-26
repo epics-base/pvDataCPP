271fec7f5e8e4cd8945e635febea45c0c1cf4827
Michael Davidsaver <mdavidsaver@gmail.com>
2018-04-26 12:11:56 -0700

    printer.cpp: clean whitespace

--- src/factory/printer.cpp
+++ src/factory/printer.cpp
@@ -3,7 +3,7 @@
  * Copyright information and license terms for this software can be
  * found in the file LICENSE that is included with the distribution
  */
- 
+
 #include <deque>
 
 #define epicsExportSharedSymbols
@@ -15,30 +15,30 @@ namespace epics { namespace pvData {
 
 namespace format
 {
-    static int indent_index = std::ios_base::xalloc();
+static int indent_index = std::ios_base::xalloc();
 
-    long& indent_value(std::ios_base& ios)
-    {
-      return ios.iword(indent_index);
-    }
+long& indent_value(std::ios_base& ios)
+{
+    return ios.iword(indent_index);
+}
 
-    std::ostream& operator<<(std::ostream& os, indent_level const& indent)
-	{
-		indent_value(os) = indent.level;
-		return os;
-	}
+std::ostream& operator<<(std::ostream& os, indent_level const& indent)
+{
+    indent_value(os) = indent.level;
+    return os;
+}
 
-	std::ostream& operator<<(std::ostream& os, indent const&)
-	{
-		long il = indent_value(os);
-		std::size_t spaces = static_cast<std::size_t>(il) * 4;
-		return os << string(spaces, ' ');
-	}
+std::ostream& operator<<(std::ostream& os, indent const&)
+{
+    long il = indent_value(os);
+    std::size_t spaces = static_cast<std::size_t>(il) * 4;
+    return os << string(spaces, ' ');
+}
 
-	array_at_internal operator<<(std::ostream& str, array_at const& manip)
-	{
-		return array_at_internal(manip.index, str);
-	}
+array_at_internal operator<<(std::ostream& str, array_at const& manip)
+{
+    return array_at_internal(manip.index, str);
+}
 };
 
 }}
