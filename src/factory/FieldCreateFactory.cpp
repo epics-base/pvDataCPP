dd24b2ad75847ea728553d9eba5002551c7b4525
Michael Davidsaver <mdavidsaver@gmail.com>
2019-09-30 18:24:13 -0700

    dynamic_cast may not be defined during dtor

--- src/factory/FieldCreateFactory.cpp
+++ src/factory/FieldCreateFactory.cpp
@@ -58,9 +58,8 @@ struct FieldCreate::Helper {
 
         std::pair<cache_t::iterator, cache_t::iterator> itp(create->cache.equal_range(hash));
         for(; itp.first!=itp.second; ++itp.first) {
-            Field* cent(itp.first->second);
-            FLD* centx(dynamic_cast<FLD*>(cent));
-            if(centx && compare(*centx, *ent)) {
+            Field* const cent(itp.first->second);
+            if(compare(*cent, *ent)) {
                 try{
                     ent = std::tr1::static_pointer_cast<FLD>(cent->shared_from_this());
                     return;
